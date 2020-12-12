b2d05f85200bfa13f6c5f08d10e1e7ba5dd7aa50
Ben Cohen <benc@moveo.co.il>
2020-12-12 21:10:22 +0200

    small changes to q3

--- q3.py
+++ q3.py
@@ -11,28 +11,28 @@ def sum_homology(S, T):
     arr_first = np.empty(len(T) + 1, dtype=float)
     arr_second = np.empty(len(T) + 1, dtype=float)
 
-    arr_first[0] = 0.0
+    arr_first[0] = 1
 
     for x in range(1, len(arr_first)):
-        arr_first[x] = arr_first[x - 1] + math.log(p_indel)*-1
+        arr_first[x] = arr_first[x - 1] * p_indel #math.log(p_indel)*-1
 
     for idx_S in range(1, len(S) + 1):
         for idx_T in range(len(T) + 1):
             if (idx_T == 0):
-                arr_second[idx_T] = arr_first[idx_T] + math.log(p_indel)*-1
+                arr_second[idx_T] = arr_first[idx_T] * p_indel #math.log(p_indel)*-1
             else:
                 if (S[idx_S - 1] == T[idx_T - 1]):
-                    misORmatchProb = arr_first[idx_T - 1] + math.log(p_match)*-1
+                    misORmatchProb = arr_first[idx_T - 1] * p_match #math.log(p_match)*-1
                 else:
-                    misORmatchProb = arr_first[idx_T - 1] + math.log(p_mismatch)*-1
+                    misORmatchProb = arr_first[idx_T - 1] * p_mismatch #math.log(p_mismatch)*-1
 
                 arr_second[idx_T] = sum([misORmatchProb,
-                                         arr_second[idx_T - 1] + math.log(p_indel)*-1,
-                                         arr_first[idx_T] + math.log(p_indel)*-1])
+                                         arr_second[idx_T - 1] * p_indel, #math.log(p_indel)*-1,
+                                         arr_first[idx_T] * p_indel]) #math.log(p_indel)*-1])
 
         # print("First array: ", arr_first)
 
-        arr_first = arr_second
+        arr_first = arr_second.copy()
         arr_second = np.empty(len(T) + 1, dtype=float)
 
     print("\nThe sumHomology is: ", arr_first[-1])
