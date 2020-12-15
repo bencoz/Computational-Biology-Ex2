d7a12a6921d7a9af92d457a55662bd75578d4f2d
Ben Cohen <benc@moveo.co.il>
2020-12-15 16:54:05 +0200

    kim refactor

--- q2.py
+++ q2.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def lcs(X, Y):
     m = len(X)
     n = len(Y)
@@ -17,6 +18,51 @@ def lcs(X, Y):
 
     return max_score
 
-S, T = 'TAGTCACG', 'AGACTGTC'
 
-print("Length of LCS is ", lcs(S, T))
+MAX = 11
+
+
+# Return LCS with at most 2 changes allowed.
+def lcs_at_most_2_mismatches(X, Y):
+    m = len(X)
+    n = len(Y)
+    max_score = 0
+    L0 = np.zeros((m + 1, n + 1))
+    L1 = np.zeros((m + 1, n + 1))
+    L2 = np.zeros((m + 1, n + 1))
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if X[i - 1] == Y[j - 1]:
+                L0[i][j] = L0[i - 1][j - 1] + 1
+            else:
+                L0[i][j] = 0
+            if L0[i][j] > max_score:
+                max_score = L0[i][j]
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if X[i - 1] == Y[j - 1]:
+                L1[i][j] = L1[i - 1][j - 1] + 1
+            else:
+                L1[i][j] = L0[i - 1][j - 1] + 1
+            if L1[i][j] > max_score:
+                max_score = L1[i][j]
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if X[i - 1] == Y[j - 1]:
+                L2[i][j] = L2[i - 1][j - 1] + 1
+            else:
+                L2[i][j] = L1[i - 1][j - 1] + 1
+            if L2[i][j] > max_score:
+                max_score = L2[i][j]
+
+    return max_score
+
+
+S, T = 'AATTAAAAGCC', 'AACCAAAATCC'
+# print("Length of LCS is ", lcs(S, T))
+
+score = lcs_at_most_2_mismatches(S, T)
+print("Length of LCS with 2 changes is ", score)
--- q3.py
+++ q3.py
@@ -1,45 +1,65 @@
-import itertools
 import numpy as np
-import math
 
-""" Question 3 b"""
+
 def sum_homology(S, T):
-    p_match = 0.2
-    p_mismatch = 0.0125
-    p_indel = 0.00625
+    p_match = 32 / 160
+    p_mismatch = 2 / 160
+    p_indel = 1 / 160
 
-    arr_first = np.empty(len(T) + 1, dtype=float)
-    arr_second = np.empty(len(T) + 1, dtype=float)
+    arr_first = np.zeros(min(len(T) + 1, len(S) + 1), dtype=float)
+    arr_second = np.zeros(min(len(T) + 1, len(S) + 1), dtype=float)
 
     arr_first[0] = 1
 
-    for x in range(1, len(arr_first)):
-        arr_first[x] = arr_first[x - 1] * p_indel #math.log(p_indel)*-1
+    if len(S) < len(T):
+        temp = S
+        S = T
+        T = temp
 
     for idx_S in range(1, len(S) + 1):
-        for idx_T in range(len(T) + 1):
-            if (idx_T == 0):
-                arr_second[idx_T] = arr_first[idx_T] * p_indel #math.log(p_indel)*-1
-            else:
-                if (S[idx_S - 1] == T[idx_T - 1]):
-                    misORmatchProb = arr_first[idx_T - 1] * p_match #math.log(p_match)*-1
-                else:
-                    misORmatchProb = arr_first[idx_T - 1] * p_mismatch #math.log(p_mismatch)*-1
-
-                arr_second[idx_T] = sum([misORmatchProb,
-                                         arr_second[idx_T - 1] * p_indel, #math.log(p_indel)*-1,
-                                         arr_first[idx_T] * p_indel]) #math.log(p_indel)*-1])
-
-        # print("First array: ", arr_first)
-
+        for idx_T in range(1, len(T) + 1):
+            arr_second[idx_T] = sum([arr_first[idx_T - 1] * (p_match if S[idx_S - 1] == T[idx_T - 1] else p_mismatch), \
+                                     arr_second[idx_T - 1] * p_indel, \
+                                     arr_first[idx_T] * p_indel])
         arr_first = arr_second.copy()
-        arr_second = np.empty(len(T) + 1, dtype=float)
+        arr_second = np.zeros(len(T) + 1, dtype=float)
+    return arr_first[-1]
 
-    print("\nThe sumHomology is: ", arr_first[-1])
+
+def NW_from_hw1(a, b, match_score=2, gap_cost=-3, mismatch_score=-2):
+    H = np.ones((len(a) + 1, len(b) + 1), np.int) * (-np.inf)
+
+    for i in range(0, H.shape[0]):
+        for j in range(0, H.shape[1]):
+            if i == 0 and j == 0:
+                H[i, j] = 0.0
+            elif i == 0 and j != 0:
+                H[i, j] = H[i, j - 1] + gap_cost
+            elif j == 0 and i != 0:
+                H[i, j] = H[i - 1, j] + gap_cost
+            else:
+                H[i, j] = max(H[i - 1, j - 1] + (match_score if a[i - 1] == b[j - 1] else mismatch_score), \
+                              H[i, j - 1] + gap_cost, \
+                              H[i - 1, j] + gap_cost)
+    return H[-1, -1]
+
+
+def find_max_n(S, T):
+    x = sum_homology(S, T)
+    n = 0
+    while x != 0:
+        n += 1
+        S = 'T' + S
+        x = sum_homology(S, T)
+    return [n, S]
 
 
 S, T = 'ATAAGGCATTGACCGTATTGCCAA', 'CCCATAGGTGCGGTAGCC'
 
-sum_homology(S, T)
+print("The sumHomology is: ", sum_homology(S, T))
 
+n = find_max_n(S, T)[0]
+new_S = find_max_n(S, T)[1]
 
+print("The number of T's added until probability is 0 is: ", n)
+print("The score of the global alignment from hw 1 with the new sequence is: ", NW_from_hw1(new_S, T))
